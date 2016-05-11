6afb5a0a91b9749dd02b9f6e7979611c4589bc9f
Kure <christian.parobek@gmail.com>
2016-05-11 15:24:59 -0400

    made a gff prepping script for PopGenome; made sure that this includes just nuclear genes that we didn't exclude in our variant-calling analysis; made sure it represents the same number of genes that Nick B is simulating

---
+++ PopGenome/gff_prep/gff_prepper.sh
@@ -0,0 +1,61 @@
+## Started 11 May 2016
+## Christian Parobek
+## To subset my P. falciparum and P. vivax gffs 
+## Prior to splitting and use in PopGenome
+
+pf_gff=/proj/julianog/refs/Pf3D7_v13.0/PlasmoDB-13.0_Pfalciparum3D7.gff
+pv_gff=/proj/julianog/refs/PvSAL1_v13.0/PlasmoDB-13.0_PvivaxSal1.gff
+nick=/proj/julianog/users/NickB/Projects/cambodiaWGS/data\&output
+
+
+################################
+######### For P. vivax #########
+################################
+
+grep "gene\|##" $pv_gff | grep -v "AAKM\|NC\|FASTA" > PvSal1_genes.gff
+	## First, make a GFF with only gene entries and header
+	## Excluding entries on AAKM contigs, mito, and the random "FASTA" line
+
+
+bedtools intersect -v -a PvSal1_genes.gff -b $nick/subtelo_60k_altered.bed |\
+ bedtools intersect -v -a "stdin" -b $nick/neafseyExclude_altered.bed > PvSal1_filtered.gff
+	## Then remove subtelomeres and Neafsey genes
+	## Then make sure to paste back in the header
+
+## Then need to split GFF by chromosome
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14
+do
+
+grep "^#\|Pv_Sal1_chr$i" PvSal1_filtered.gff > gff/chr$i.gff
+
+done
+
+#################################
+####### For P. falciparum #######
+#################################
+
+grep "gene\|##" $pf_gff | grep -v "IRAB\|M76611\|FASTA" > Pf3D7_genes.gff
+	## First, make a GFF with only gene entries and header
+	## Excluding entries on AAKM contigs, mito, and the random "FASTA" line
+
+bedtools intersect -v -a Pf3D7_genes.gff -b $nick/subtelomeres_pf3d7.bed |\
+ bedtools intersect -v -a "stdin" -b $nick/VAR_STEVOR_RIFIN_pf3d7.bed > Pf3D7_filtered.gff
+	## Then remove subtelomeres and Neafsey genes
+	## Then make sure to paste back in the header
+
+## Then need to split GFF by chromosome
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14
+do
+
+grep "^#\|Pf3D7_$i\_v3" Pf3D7_filtered.gff > gff/chr$i.gff
+
+done
+
+## Then replace the original split GFF files in popgenome folder with these split gffs
+
+
+
+
+
+
+
